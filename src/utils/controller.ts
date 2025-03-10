9b5a06de55d8a1a5f38a6cb3c0af63c81a37feef
ddisan <san.apisitn@gmail.com>
2025-03-11 00:30:11 +0700

    static import

--- src/utils/controller.ts
+++ src/utils/controller.ts
@@ -1,86 +1,61 @@
-import {
-	Client,
-	Collection,
-	GatewayIntentBits,
-	type Interaction,
-	REST,
-	type RESTPostAPIApplicationCommandsJSONBody,
-	Routes,
-	type SlashCommandBuilder,
-} from "discord.js";
 import { env } from "@/env";
+import {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  type Interaction,
+  REST,
+  type RESTPostAPIApplicationCommandsJSONBody,
+  Routes,
+  type SlashCommandBuilder,
+} from "discord.js";
 
-import { readdirSync } from "node:fs";
-import { readdir } from "node:fs/promises";
-import path from "node:path";
 import { Logs } from "@/utils/logs";
 
 Logs.info("Running in", env.NODE_ENV);
 
 export const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.MessageContent,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-readdirSync(path.join(__dirname, "../events/"))
-	.filter((file) => file.endsWith(".ts"))
-	.forEach((file) => {
-		import(`../events/${file.replace(".ts", "")}`);
-	});
+import "@/events/clientReady";
+import "@/events/guildMemberAdd";
+import "@/events/interactionCreate";
+import "@/events/messageCreate";
 
 export const commands = new Collection<
-	string,
-	{
-		data: SlashCommandBuilder;
-		execute: (interaction: Interaction) => void | Promise<void>;
-	}
+  string,
+  {
+    data: SlashCommandBuilder;
+    execute: (interaction: Interaction) => void | Promise<void>;
+  }
 >();
 const slashCommands: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
-readdir(path.join(__dirname, "../commands/"))
-	.then((files) => files.filter((file) => file.endsWith(".ts")))
-	.then(async (files) => {
-		for (const file of files) {
-			const { data, execute } = await import(
-				`../commands/${file.replace(".ts", "")}`
-			);
+import * as ping from "@/commands/ping";
+commands.set(ping.data.name, { data: ping.data, execute: ping.execute });
+slashCommands.push(ping.data.toJSON());
 
-			if (data && execute) {
-				commands.set(data.name, { data, execute });
-				slashCommands.push(data.toJSON());
-			} else {
-				Logs.warning(
-					`The command at commands/${file} is missing a required "data" or "execute" property.`,
-				);
-			}
-		}
-	})
-	.then(() => {
-		putSlashCommands();
-	});
+try {
+  const rest = new REST().setToken(env.DISCORD_TOKEN);
 
-const putSlashCommands = async () => {
-	try {
-		const rest = new REST().setToken(env.DISCORD_TOKEN);
+  Logs.info(
+    `Started refreshing ${slashCommands.length} application (/) commands.`
+  );
 
-		Logs.info(
-			`Started refreshing ${slashCommands.length} application (/) commands.`,
-		);
+  await rest.put(Routes.applicationGuildCommands(env.CLIENT_ID, env.GUILD_ID), {
+    body: slashCommands,
+  });
 
-		await rest.put(
-			Routes.applicationGuildCommands(env.CLIENT_ID, env.GUILD_ID),
-			{ body: slashCommands },
-		);
-
-		Logs.info(
-			`Successfully reloaded ${slashCommands.length} application (/) commands.`,
-		);
-	} catch (error) {
-		Logs.error("Routes.applicationGuildCommands", error);
-	}
-};
+  Logs.info(
+    `Successfully reloaded ${slashCommands.length} application (/) commands.`
+  );
+} catch (error) {
+  Logs.error("Routes.applicationGuildCommands", error);
+}
